dbb733c13b9d7a0020e2adf021fdb8dc34159aad
Fiuzah <isabelle.fiuza@escola.pr.gov.br>
2026-06-18 08:17:11 -0300

    aaaa

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function curtir() {
     let curtidas = document.querySelector("span")
     curtidas.textContent++;
 }
+
+//Botão de tamanho
+const tamanho = document.getElementById("tamanho")
+tamanho.addEventListener("click", aumentarFonte)
+let fonte = 16;
+//função
+function aumentarFonte(){
+    fonte++;
+    document.body.style.fontSize = fonte + "px";
+}
+
