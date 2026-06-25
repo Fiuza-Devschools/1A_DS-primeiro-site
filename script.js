083cf9f67be13202be142e79e9eb7ace6d06d662
Fiuzah <isabelle.fiuza@escola.pr.gov.br>
2026-06-25 08:53:06 -0300

    ...

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function curtir() {
     curtidas.textContent++;
 }
 
-//Botao de tamanho
+//BOTAO A+
 const tamanho = document.getElementById("tamanho")
 tamanho.addEventListener("click", aumentarFonte)
 let fonte = 16;
@@ -21,3 +21,21 @@ const contraste = document.getElementById("contraste")
  contraste.addEventListener("click",contraste_amarelo )
  //FUNÇAO
 function contraste_amarelo(){ document.body.style.color = "yellow" }
+
+// BOTAO A-
+const diminuir = document.getElementById("diminuir");
+
+diminuir.addEventListener("click", diminuirFonte);
+
+function diminuirFonte(){
+
+    document.querySelectorAll("p, h1, h2").forEach(texto => {
+
+        let tamanhoAtual = parseInt(
+            window.getComputedStyle(texto).fontSize
+        );
+
+        texto.style.fontSize = (tamanhoAtual - 2) + "px";
+    });
+
+}
